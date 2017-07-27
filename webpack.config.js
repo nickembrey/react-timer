3ab63c324c0f47bcde80b520363f7e4d098192ec
Nick Embrey <cne.embrey@gmail.com>
2017-07-26 19:27:16 -0700

    Add basic clock tests

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,7 @@ module.exports = {
       Nav: 'app/components/Nav.jsx',
       Countdown: 'app/components/Countdown.jsx',
       Timer: 'app/components/Timer.jsx',
+      Clock: 'app/components/Clock.jsx',
       applicationStyles: 'app/styles/app.scss'
     },
     extensions: ['', '.js', '.jsx']
