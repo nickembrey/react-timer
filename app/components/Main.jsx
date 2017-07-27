735ce560cd8e44820715cba98f155747ecf03ef6
Nick Embrey <cne.embrey@gmail.com>
2017-07-26 17:22:12 -0700

    Style nav bar

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,10 +1,12 @@
 var React = require('react');
+var Nav = require('Nav');
 
 var Main = (props) => {
   return (
     <div>
       <div>
         <div>
+          <Nav />
           <p>Main.jsx rendered</p>
           {props.children}
         </div>
--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,7 @@ module.exports = {
     root: __dirname,
     alias: {
       Main: 'app/components/Main.jsx',
+      Nav: 'app/components/Nav.jsx',
       applicationStyles: 'app/styles/app.scss'
     },
     extensions: ['', '.js', '.jsx']
